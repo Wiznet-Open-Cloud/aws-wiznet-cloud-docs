73f86b07408d754448dfa2f0f845309fbb65fbf9
dlruddms5619@gmail.com <dlruddms5619@gmail.com>
2022-10-21 08:15:35 +0900

    Modify url path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,8 +9,8 @@ const rlc = require("remark-link-card");
 const config = {
   title: "WIZnet Cloud Docs",
   tagline: "Guides and Docs for wizcloud.io", // 나중에 도메인 변경
-  url: "https://Wiznet-Open-Cloud.github.io", // docs 도메인으로 변경
-  baseUrl: "/aws-wiznet-cloud-docs/",
+  url: "https://wiznet-open-cloud.github.io/aws-wiznet-cloud-docs/", // docs 도메인으로 변경
+  baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/wizfavicon.ico",
